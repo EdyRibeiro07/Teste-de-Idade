d83a0f2c2e00ad4dd51870d7ae3bb223b8be9d01
Edy Ribeiro <edyribeiro07@gmail.com>
2022-02-20 23:20:07 -0300

    Progresso

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
 function verificar(){
     var data = new Date()
-    var anoatual = data.getFullYear
+    var anoa = data.getFullYear()
 
-    var ano = window.document.querySelector('input#ano')
-    var sexo = window.document.querySelector('.lopc')
-    var img = window.document.querySelector('img')
-    var msg = window.document.querySelector('div.msg')
-
-
-    if ((anoatual - ano) <= 11 && sexo == 'Masculino'){
-        img.src = 'img/01.jpg'
-        msg.textContent = `Identificamos uma Criança, Homem, de ${ano} anos`
-    }else if ((anoatual - ano) <= 11 && sexo == 'Femenino'){
-        img.src = 'img/02.jpg'
-        msg.textContent = `Identificamos uma Criança, Mulher, de ${ano} anos`
-    }
+    var anof = window.document.querySelector('input#ano')
     
-}
 
-/*
-0 - 11
-12 - 16
-17 - 28
-29 - 49
-50 - 69
-70
-*/
+    
+
+    if (anof.value.length == 0 || anof.value > anoa){
+        window.alert('Erro, Verifique e Tente Novamente')
+    }else{
+        var idade = anoa - anof.value
+
+        var sexo = window.document.querySelector('input.lopc')
+        var img = window.document.querySelector('img')
+        var msg = window.document.querySelector('p')
+
+        msg.innerText = `${genero}, de ${Idade} Anos`
+    }
+
+}

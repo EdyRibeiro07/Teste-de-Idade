d587291f27171207cf543c84cbf50d24eca4f533
Edy Ribeiro <edyribeiro07@gmail.com>
2022-02-20 23:42:49 -0300

    Js finalizado

--- script.js
+++ script.js
@@ -12,16 +12,46 @@ function verificar(){
     }else{
         var idade = anoa - anof.value
 
-        var sexo = window.document.querySelector('input.lopc')
+        var sexo = window.document.getElementsByName('sexo')
         var img = window.document.querySelector('img')
         var msg = window.document.querySelector('p')
         
 
-        if(sexo[0] == True){
-
+        if(sexo[0].checked){
+            genero = 'Homem'
+        }else if(sexo[1].checked){
+            genero = 'Mulher'
         }
 
-        msg.innerText = ` de ${idade} Anos`
+        msg.innerText = `${genero} de ${idade} Anos`
+
+        if (idade < 12 && genero == 'Homem'){
+            img.src = 'img/01.jpg'
+        }else if(idade < 12 && genero == 'Mulher'){
+            img.src = 'img/02.jpg'
+        }else if (idade > 11 && idade < 18 && genero == 'Homem'){
+            img.src = 'img/03.jpg'
+        }else if(idade > 11 && idade < 18 &&genero == 'Mulher'){
+            img.src = 'img/04.jpg'
+        }else if (idade > 17 && idade < 29 && genero == 'Homem'){
+            img.src = 'img/05.jpg'
+        }else if(idade > 17 && idade < 29 &&genero == 'Mulher'){
+            img.src = 'img/06.jpg' 
+        }else if (idade > 28 && idade < 50 && genero == 'Homem'){
+            img.src = 'img/07.jpg'
+        }else if(idade > 28 && idade < 50 &&genero == 'Mulher'){
+            img.src = 'img/08.jpg'
+        }else if (idade > 49 && idade < 70 && genero == 'Homem'){
+            img.src = 'img/09.jpg'
+        }else if(idade > 49 && idade < 70 &&genero == 'Mulher'){
+            img.src = 'img/10.jpg'
+        }else if (idade > 69 && genero == 'Homem'){
+            img.src = 'img/11.jpg'
+        }else if(idade > 69 && genero == 'Mulher'){
+            img.src = 'img/12.jpg'
+        }
     }
+
+
 
 }
